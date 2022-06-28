9ba5508be6025c7979006ca2104dd71a703f6113
nxgisaexe <nxgisa.exe05@gmail.com>
2022-06-28 20:16:06 +0100

    Initial Commit

---
+++ Lenny's teleportation ride!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lenny's teleportation ride!
+//
+//  Created by nicole lien on 28/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
